9ab96c497727f04ac29275edf11c952ea209e20f
Ehsan Jahromi <ehsan.enj@gmail.com>
2016-12-26 15:06:21 -0800

    added badge values to viewDidLoad

---
+++ ClearNotificationsDemo/MessagesViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MessagesViewController.swift
+//  ClearNotificationsDemo
+//
+//  Created by Ehsan Jahromi on 12/26/16.
+//  Copyright © 2016 Ehsan Jahromi. All rights reserved.
+//
+
+import UIKit
+
+class MessagesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tabBarItem.badgeValue = String(1)
+    }
+    
+
+
+}
---
+++ ClearNotificationsDemo/NotificationsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationsViewController.swift
+//  ClearNotificationsDemo
+//
+//  Created by Ehsan Jahromi on 12/26/16.
+//  Copyright © 2016 Ehsan Jahromi. All rights reserved.
+//
+
+import UIKit
+
+class NotificationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tabBarItem.badgeValue = String(1)
+    }
+
+
+}
