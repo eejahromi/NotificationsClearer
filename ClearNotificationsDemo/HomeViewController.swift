8606720b82729a3f1470bd540dbf78300a75003b
Ehsan Jahromi <ejahromi@fieldtechnologies.com>
2017-01-02 16:37:03 -0800

    removed extra viewControllers

--- ClearNotificationsDemo/HomeViewController.swift
+++ ClearNotificationsDemo/HomeViewController.swift
@@ -10,12 +10,30 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var stepper: UIStepper!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.title = "Home"
-        self.navigationController?.tabBarItem.badgeValue = String(1)
+        
+        switch self.tabBarController!.selectedIndex {
+            case 1:
+                self.title = "Messages"
+            case 2:
+                self.title = "Notifications"
+            default:
+                self.title = "Home"
+        }
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.stepper.value = 0.0
     }
 
+    @IBAction func updateBadgeCount(_ sender: UIStepper) {
+        self.navigationController?.tabBarItem.badgeValue = String(Int(sender.value))
+    }
 
 }
